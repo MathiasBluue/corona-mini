3e1b5500c253b93d91a2293babcdb3c452230592
MathiasBluue <jacobs72@wwu.edu>
2020-04-05 00:17:52 -0600

    Added Player.
    -Added Player Object
    -Added Player Script
    -Added sprites for Player:
        -standing
        -stepping right
        -stepping left

---
+++ CoronaMini Project/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("Player has entered the game. Hello world.");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
